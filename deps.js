1b55e7e7b48c1f4fed8a3505640360be3634764e
cnrdh <cnrdh@users.noreply.github.com>
2021-04-30 13:14:57 +0200

    Update

--- deps.js
+++ deps.js
@@ -2,5 +2,5 @@ export { commonOptions } from "https://deno.land/x/newline@v0.1.0/args.js";
 export { ndmapcommand } from "https://deno.land/x/newline@v0.1.0/nd-map/command.js";
 export { jsongenerator } from "https://deno.land/x/newline@v0.1.0/generator/json-gen.js";
 export { csvgenerator } from "https://deno.land/x/newline@v0.1.0/nd-csv/csv-gen.js";
-export { parse as parseArgs } from "https://deno.land/std@0.93.0/flags/mod.ts";
-export { generate } from "https://deno.land/std@0.93.0/uuid/v5.ts";
+export { parse as parseArgs } from "https://deno.land/std@0.95.0/flags/mod.ts";
+export { generate } from "https://deno.land/std@0.95.0/uuid/v5.ts";
